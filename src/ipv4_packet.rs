9edb57d3ec1b147713e2515ed70d094eddcd41a4
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-01-04 17:37:01 -0500

    Include header in `Ipv4Packet`, use DI for protocol

--- src/ipv4_packet.rs
+++ src/ipv4_packet.rs
@@ -6,20 +6,49 @@ use std::{fmt, net::Ipv4Addr};
 
 pub const IPV4_HEADER_MIN_LEN: u8 = 20;
 
+pub trait ProtocolHandlerFactory<'a>:
+    FnOnce(Protocol, &'a [u8]) -> Result<Box<dyn ProtocolHandler + 'a>, String>
+{
+}
+
+impl<'a, T> ProtocolHandlerFactory<'a> for T where
+    T: FnOnce(Protocol, &'a [u8]) -> Result<Box<dyn ProtocolHandler + 'a>, String>
+{
+}
+
 pub struct Ipv4Packet<'a> {
-    ipv4_header: Ipv4Header,
+    total_len: u16,
+    protocol: Protocol,
+    src_ip: Ipv4Addr,
+    dst_ip: Ipv4Addr,
     protocol_handler: Box<dyn ProtocolHandler + 'a>,
 }
 
 impl<'a> Ipv4Packet<'a> {
-    pub fn parse(data: &'a [u8]) -> Result<Self, String> {
-        let ipv4_header = Ipv4Header::parse(data)?;
+    pub fn parse(
+        data: &'a [u8],
+        protocol_handler_factory: impl ProtocolHandlerFactory<'a>,
+    ) -> Result<Self, String> {
+        let n = data.len();
+        if n < IPV4_HEADER_MIN_LEN.into() {
+            return Err(format!("Too short for IPv4 header ({n} bytes)"));
+        }
 
-        let protocol_handler = ipv4_header
-            .protocol
-            .parse_data(&data[ipv4_header.ihl_bytes..])?;
+        let version = data[0] >> 4;
+        if version != 4 {
+            return Err(format!("Non-IPv4 packet (version {version})"));
+        }
 
-        Ok(Self { ipv4_header, protocol_handler })
+        let ihl_bytes = usize::from(data[0] & 0xF) * 4; // Convert 32-bit words to bytes
+        let protocol = Protocol::from(data[9]);
+
+        Ok(Self {
+            total_len: u16::from_be_bytes([data[2], data[3]]),
+            protocol,
+            src_ip: Ipv4Addr::new(data[12], data[13], data[14], data[15]),
+            dst_ip: Ipv4Addr::new(data[16], data[17], data[18], data[19]),
+            protocol_handler: protocol_handler_factory(protocol, &data[ihl_bytes..])?,
+        })
     }
 
     pub fn create_reply(&self) -> Option<([u8; ETHERNET_MTU], usize)> {
@@ -32,11 +61,11 @@ impl<'a> Ipv4Packet<'a> {
         reply[4..6].copy_from_slice(&[0x00, 0x00]); // Identification
         reply[6..8].copy_from_slice(&[0x40, 0x00]); // Flags + Fragment offset (Don't Fragment)
         reply[8] = 64; // TTL
-        reply[9] = self.ipv4_header.protocol.into(); // Protocol
+        reply[9] = self.protocol.into(); // Protocol
 
         // Swap src and dst IP addresses
-        reply[12..16].copy_from_slice(&self.ipv4_header.dst_ip.octets());
-        reply[16..20].copy_from_slice(&self.ipv4_header.src_ip.octets());
+        reply[12..16].copy_from_slice(&self.dst_ip.octets());
+        reply[16..20].copy_from_slice(&self.src_ip.octets());
 
         // Let protocol handler fill in protocol-specific data and calculate total length
         let total_len = self.protocol_handler.write_reply(&mut reply)?;
@@ -58,48 +87,10 @@ impl<'a> Ipv4Packet<'a> {
 
 impl fmt::Display for Ipv4Packet<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}\n{}", self.ipv4_header, self.protocol_handler)
-    }
-}
-
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-struct Ipv4Header {
-    ihl_bytes: usize,
-    total_len: u16,
-    protocol: Protocol,
-    src_ip: Ipv4Addr,
-    dst_ip: Ipv4Addr,
-}
-
-impl Ipv4Header {
-    fn parse(data: &[u8]) -> Result<Self, String> {
-        let n = data.len();
-
-        if n < IPV4_HEADER_MIN_LEN.into() {
-            return Err(format!("Too short for IPv4 header ({n} bytes)"));
-        }
-
-        let version = data[0] >> 4;
-        if version != 4 {
-            return Err(format!("Non-IPv4 packet (version {version})"));
-        }
-
-        Ok(Self {
-            ihl_bytes: usize::from(data[0] & 0xF) * 4, // Convert 32-bit words to bytes
-            total_len: u16::from_be_bytes([data[2], data[3]]),
-            protocol: data[9].into(),
-            src_ip: Ipv4Addr::new(data[12], data[13], data[14], data[15]),
-            dst_ip: Ipv4Addr::new(data[16], data[17], data[18], data[19]),
-        })
-    }
-}
-
-impl fmt::Display for Ipv4Header {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "IPv4 | {} bytes | {} | {} -> {}",
-            self.total_len, self.protocol, self.src_ip, self.dst_ip,
+            "IPv4 | {} bytes | {} | {} -> {}\n{}",
+            self.total_len, self.protocol, self.src_ip, self.dst_ip, self.protocol_handler,
         )
     }
 }
@@ -108,8 +99,34 @@ impl fmt::Display for Ipv4Header {
 mod tests {
     use super::*;
 
+    struct MockProtocolHandler {
+        return_val: Option<u16>,
+    }
+
+    impl ProtocolHandler for MockProtocolHandler {
+        fn write_reply(&self, _reply: &mut [u8; ETHERNET_MTU]) -> Option<u16> { self.return_val }
+    }
+
+    impl fmt::Display for MockProtocolHandler {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "mock protocol handler with return value {:#?}",
+                self.return_val
+            )
+        }
+    }
+
+    /// Creates a factory function that returns a `ProtocolHandler` whose `write_reply` method
+    /// returns `return_val`.
+    fn make_factory_returning_mock_handler_returning<'a>(
+        return_val: Option<u16>,
+    ) -> impl ProtocolHandlerFactory<'a> {
+        move |_protocol, _data| Ok(Box::new(MockProtocolHandler { return_val }))
+    }
+
     #[test]
-    fn ipv4_header_correctly_parses_valid_packet() {
+    fn correctly_parses_valid_packet() {
         #[rustfmt::skip]
         let data = [
             0x45, 0x00, 0x00, 0x3c,  // Version 4, IHL 5, TOS 0, Total Length 60
@@ -119,30 +136,26 @@ mod tests {
             172, 16, 10, 12,         // Dest IP: 172.16.10.12
         ];
 
-        assert!(Ipv4Header::parse(&data).is_ok_and(|header| {
-            assert_eq!(
-                header,
-                Ipv4Header {
-                    ihl_bytes: 20,
-                    total_len: 60,
-                    protocol: Protocol::Tcp,
-                    src_ip: Ipv4Addr::new(192, 168, 1, 100),
-                    dst_ip: Ipv4Addr::new(172, 16, 10, 12),
-                }
-            );
+        let mock = make_factory_returning_mock_handler_returning(None);
 
+        assert!(Ipv4Packet::parse(&data, mock).is_ok_and(|packet| {
+            assert_eq!(packet.total_len, 60);
+            assert_eq!(packet.protocol, Protocol::Tcp);
+            assert_eq!(packet.src_ip, Ipv4Addr::new(192, 168, 1, 100));
+            assert_eq!(packet.dst_ip, Ipv4Addr::new(172, 16, 10, 12));
             true
         }));
     }
 
     #[test]
-    fn ipv4_header_parsing_fails_when_too_short() {
+    fn parsing_fails_if_too_short() {
         let data = [0x45, 0x00, 0x00]; // Only 3 bytes
-        assert!(Ipv4Header::parse(&data).is_err_and(|e| e.contains("Too short")));
+        let mock = make_factory_returning_mock_handler_returning(None);
+        assert!(Ipv4Packet::parse(&data, mock).is_err_and(|e| e.contains("Too short")));
     }
 
     #[test]
-    fn ipv4_header_parsing_fails_when_not_ipv4() {
+    fn parsing_fails_if_not_ipv4() {
         #[rustfmt::skip]
         let data = [
             0x60, 0x00, 0x00, 0x00,  // Version 6 (IPv6), not 4
@@ -153,6 +166,8 @@ mod tests {
             0x00, 0x00, 0x00, 0x01,
         ];
 
-        assert!(Ipv4Header::parse(&data).is_err_and(|e| e.contains("Non-IPv4")));
+        let mock = make_factory_returning_mock_handler_returning(None);
+
+        assert!(Ipv4Packet::parse(&data, mock).is_err_and(|e| e.contains("Non-IPv4")));
     }
 }
