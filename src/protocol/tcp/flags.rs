a4cfc3043496e98bc544503d7d77405db1350d16
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-06-08 15:38:12 -0400

    Use u8 discriminant and bitmask for `TcpFlags` enum

--- src/protocol/tcp/flags.rs
+++ src/protocol/tcp/flags.rs
@@ -2,13 +2,14 @@ use std::fmt;
 
 #[cfg_attr(test, derive(Debug, PartialEq, Eq))]
 #[derive(Clone, Copy)]
+#[repr(u8)]
 pub(super) enum TcpFlags {
-    Syn,
-    SynAck,
-    Ack,
-    FinAck,
-    Rst,
-    RstAck,
+    Syn = Self::SYN_BIT,
+    SynAck = Self::SYN_BIT | Self::ACK_BIT,
+    Ack = Self::ACK_BIT,
+    FinAck = Self::FIN_BIT | Self::ACK_BIT,
+    Rst = Self::RST_BIT,
+    RstAck = Self::RST_BIT | Self::ACK_BIT,
 }
 
 impl TcpFlags {
@@ -22,37 +23,36 @@ impl TryFrom<u8> for TcpFlags {
     type Error = String;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match (
-            value & Self::SYN_BIT != 0,
-            value & Self::ACK_BIT != 0,
-            value & Self::FIN_BIT != 0,
-            value & Self::RST_BIT != 0,
-        ) {
-            (true, false, false, false) => Ok(Self::Syn),
-            (true, true, false, false) => Ok(Self::SynAck),
-            (false, true, false, false) => Ok(Self::Ack),
-            (false, true, true, false) => Ok(Self::FinAck),
-            (false, false, false, true) => Ok(Self::Rst),
-            (false, true, false, true) => Ok(Self::RstAck),
+        /// Mask of only the bits considered by this enum.
+        const MASK: u8 =
+            TcpFlags::FIN_BIT | TcpFlags::SYN_BIT | TcpFlags::RST_BIT | TcpFlags::ACK_BIT;
 
-            (syn, ack, fin, rst) => Err(format!(
-                "Invalid TCP flag combination: SYN={syn} ACK={ack} FIN={fin} RST={rst}"
+        const SYN_ACK: u8 = TcpFlags::SynAck as u8;
+        const FIN_ACK: u8 = TcpFlags::FinAck as u8;
+        const RST_ACK: u8 = TcpFlags::RstAck as u8;
+
+        match value & MASK {
+            Self::SYN_BIT => Ok(Self::Syn),
+            Self::ACK_BIT => Ok(Self::Ack),
+            Self::RST_BIT => Ok(Self::Rst),
+
+            SYN_ACK => Ok(Self::SynAck),
+            FIN_ACK => Ok(Self::FinAck),
+            RST_ACK => Ok(Self::RstAck),
+
+            other => Err(format!(
+                "Invalid TCP flag combination: FIN={} SYN={} RST={} ACK={}",
+                other & Self::FIN_BIT != 0,
+                other & Self::SYN_BIT != 0,
+                other & Self::RST_BIT != 0,
+                other & Self::ACK_BIT != 0,
             )),
         }
     }
 }
 
 impl From<TcpFlags> for u8 {
-    fn from(value: TcpFlags) -> Self {
-        match value {
-            TcpFlags::Syn => TcpFlags::SYN_BIT,
-            TcpFlags::SynAck => TcpFlags::SYN_BIT | TcpFlags::ACK_BIT,
-            TcpFlags::Ack => TcpFlags::ACK_BIT,
-            TcpFlags::FinAck => TcpFlags::FIN_BIT | TcpFlags::ACK_BIT,
-            TcpFlags::Rst => TcpFlags::RST_BIT,
-            TcpFlags::RstAck => TcpFlags::RST_BIT | TcpFlags::ACK_BIT,
-        }
-    }
+    fn from(value: TcpFlags) -> Self { value as Self }
 }
 
 impl fmt::Display for TcpFlags {
