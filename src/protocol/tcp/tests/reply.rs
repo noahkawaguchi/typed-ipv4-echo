1b3ec87982656ec0b9100f026deaba0b6300d73e
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-07-08 15:51:44 -0400

    Set SEG.ACK to 0 for client RST packets in tests

--- src/protocol/tcp/tests/reply.rs
+++ src/protocol/tcp/tests/reply.rs
@@ -462,7 +462,7 @@ fn rst_exactly_at_rcv_nxt_cleans_up_connection_and_returns_none() -> Result<()> 
     connections.insert_established();
 
     assert_eq!(
-        client_packet(CLIENT_ISN + SYN_BYTE, SERVER_ISN + SYN_BYTE, TcpFlags::Rst, &[])
+        client_packet(CLIENT_ISN + SYN_BYTE, 0, TcpFlags::Rst, &[])
             .create_reply(&mut connections)?,
         None
     );
@@ -483,8 +483,8 @@ fn rst_within_window_but_not_at_rcv_nxt_gets_challenge_ack() -> Result<()> {
 
     // seq_num=CLIENT_ISN+4 is inside the receive window [CLIENT_ISN+1, CLIENT_ISN+1+RCV.WND), but
     // seq_num=CLIENT_ISN+4 != rcv_nxt=CLIENT_ISN+1
-    let reply = client_packet(CLIENT_ISN + 4, SERVER_ISN + SYN_BYTE, TcpFlags::Rst, &[])
-        .create_reply(&mut connections)?;
+    let reply =
+        client_packet(CLIENT_ISN + 4, 0, TcpFlags::Rst, &[]).create_reply(&mut connections)?;
 
     assert_eq!(
         reply,
@@ -513,8 +513,8 @@ fn rst_with_out_of_window_seq_is_silently_dropped() -> Result<()> {
 
     // seq_num=CLIENT_ISN-10 is just below rcv_nxt=CLIENT_ISN+1, so this RST is outside the receive
     // window
-    let reply = client_packet(CLIENT_ISN - 10, SERVER_ISN + SYN_BYTE, TcpFlags::Rst, &[])
-        .create_reply(&mut connections)?;
+    let reply =
+        client_packet(CLIENT_ISN - 10, 0, TcpFlags::Rst, &[]).create_reply(&mut connections)?;
     assert_eq!(reply, None, "Out-of-window RST should be silently dropped");
 
     assert_eq!(
