190fb4c360575d22e35a438c31b84d49271aa047
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-07-16 16:38:45 -0400

    Update non-Linux compile error message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #[cfg(not(target_os = "linux"))]
-compile_error!("This crate only supports Linux because it uses Linux APIs directly");
+compile_error!("This crate only supports Linux because it directly uses low-level Linux APIs");
 
 mod addr_pairs;
 mod checksum;
