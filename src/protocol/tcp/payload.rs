ae604cadf937259d73793b0ae981d26d3c7ad78b
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-07-21 17:50:13 -0400

    Guard against incorrect `ExactSizeIterator` length

--- src/protocol/tcp/payload.rs
+++ src/protocol/tcp/payload.rs
@@ -24,22 +24,44 @@ impl TcpPayload {
     where
         I: ExactSizeIterator<Item = u8>,
     {
-        let it = iter.into_iter();
+        // `ExactSizeIterator` is a safe trait, so it's possible for `.len()` to be implemented
+        // incorrectly. However, since this should be unlikely, check the claimed length before
+        // allocating to avoid the unnecessary allocation, and then check the actual length after
+        // allocating to guarantee the invariant.
 
-        u16::try_from(it.len())
+        Self::try_payload_len(iter.len())?
+            .map(|claimed_len| {
+                let data = iter.collect::<Rc<_>>();
+
+                if let Ok(Some(len)) = Self::try_payload_len(data.len())
+                    && len == claimed_len
+                {
+                    Ok(Self { data, len })
+                } else {
+                    Err("Attempted to create a `TcpPayload` from an incorrectly implemented \
+                         ExactSizeIterator")
+                }
+            })
+            .transpose()
+    }
+
+    /// Attempts to convert a `usize` into a `NonZeroU16`, returning `Ok(None)` if `unknown_len` is
+    /// zero.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if `unknown_len` is greater than `u16::MAX`.
+    fn try_payload_len(unknown_len: usize) -> Result<Option<NonZeroU16>, &'static str> {
+        u16::try_from(unknown_len)
             .map_err(|_| {
                 "Attempted to create a `TcpPayload` from an iterator longer than `u16::MAX`"
             })
-            .map(|maybe_zero_len| {
-                NonZeroU16::try_from(maybe_zero_len)
-                    .ok()
-                    .map(|len| Self { data: it.collect(), len })
-            })
+            .map(|maybe_zero_len| NonZeroU16::try_from(maybe_zero_len).ok())
     }
 }
 
 impl Deref for TcpPayload {
     type Target = [u8];
 
-    fn deref(&self) -> &Self::Target { self.data.as_ref() }
+    fn deref(&self) -> &Self::Target { &self.data }
 }
