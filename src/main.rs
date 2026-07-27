05c8b18d8f2bc231a36523807a70fe0a172b2913
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-07-27 15:19:42 -0400

    Tighten up `main.rs` paths and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use typed_ipv4_echo::{
+    Config, Result, server,
+    sys::{ShutdownSignal, poll, tun},
+};
+
 /// Runs an echo server that uses a TUN device to read and write IPv4 packets: TCP, UDP, and ICMP.
-fn main() -> typed_ipv4_echo::Result {
-    let shutdown = typed_ipv4_echo::sys::ShutdownSignal::install()?;
+fn main() -> Result {
+    let shutdown = ShutdownSignal::install()?;
+    let config = Config::load()?;
 
-    let config = typed_ipv4_echo::Config::load()?;
+    let mut tun = tun::attach(&config.tun_name)?;
+    println!(
+        "Attached to TUN device \"{}\"\nWaiting for packets... (Ctrl+C to stop)",
+        config.tun_name
+    );
 
-    let mut tun = typed_ipv4_echo::sys::tun::attach(&config.tun_name)?;
-    println!("Attached to TUN device {}", config.tun_name);
-
-    println!("Waiting for packets... (Ctrl+C to stop)");
-
-    typed_ipv4_echo::server::run(
-        &mut tun,
-        |fd, timeout| typed_ipv4_echo::sys::poll::readable(fd, timeout),
-        || shutdown.load(),
-        &config,
-    )
+    server::run(&mut tun, |fd, timeout| poll::readable(fd, timeout), || shutdown.load(), &config)
 }
