6edb9ab38a008ec6a8490346e6e991789164bff0
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-07-27 14:10:27 -0400

    Split crate into library and binary

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(not(target_os = "linux"))]
+compile_error!("This crate only supports Linux because it directly uses low-level Linux APIs");
+
+pub mod config;
+pub mod server;
+pub mod sys;
+
+mod addr_pairs;
+mod checksum;
+mod ipv4_header;
+mod protocol;
+mod try_ops;
+
+pub type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+/// The Maximum Transmission Unit of standard Ethernet (frames up to 1500 bytes of IP packet data).
+const ETHERNET_MTU: usize = 1500;
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,17 @@
-#[cfg(not(target_os = "linux"))]
-compile_error!("This crate only supports Linux because it directly uses low-level Linux APIs");
-
-mod addr_pairs;
-mod checksum;
-mod config;
-mod ipv4_header;
-mod protocol;
-mod server;
-mod sys;
-mod try_ops;
-
-type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
-
-/// The Maximum Transmission Unit of standard Ethernet (frames up to 1500 bytes of IP packet data).
-const ETHERNET_MTU: usize = 1500;
-
 /// Runs an echo server that uses a TUN device to read and write IPv4 packets: TCP, UDP, and ICMP.
-fn main() -> Result {
-    let shutdown = sys::ShutdownSignal::install()?;
+fn main() -> typed_ipv4_echo::Result {
+    let shutdown = typed_ipv4_echo::sys::ShutdownSignal::install()?;
 
-    let config = config::load()?;
+    let config = typed_ipv4_echo::config::load()?;
 
-    let mut tun = sys::tun::attach(&config.tun_name)?;
+    let mut tun = typed_ipv4_echo::sys::tun::attach(&config.tun_name)?;
     println!("Attached to TUN device {}", config.tun_name);
 
     println!("Waiting for packets... (Ctrl+C to stop)");
 
-    server::run(
+    typed_ipv4_echo::server::run(
         &mut tun,
-        |fd, timeout| sys::poll::readable(fd, timeout),
+        |fd, timeout| typed_ipv4_echo::sys::poll::readable(fd, timeout),
         || shutdown.load(),
         &config,
     )
